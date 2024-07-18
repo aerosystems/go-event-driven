2abe926791944e0f8a7b29ebf18e31c74c3c7d43
aerosystems <art_k0st@icloud.com>
2024-07-19 00:21:20 +0300

    done 05-events/02-marshaling

---
+++ 05-events/02-marshalling/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"time"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/redis/go-redis/v9"
+)
+
+type PaymentCompleted struct {
+	PaymentID   string `json:"payment_id"`
+	OrderID     string `json:"order_id"`
+	CompletedAt string `json:"completed_at"`
+}
+
+type OrderConfirmed struct {
+	OrderID     string `json:"order_id"`
+	ConfirmedAt string `json:"confirmed_at"`
+}
+
+func main() {
+	logger := watermill.NewStdLogger(false, false)
+
+	router, err := message.NewRouter(message.RouterConfig{}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	sub, err := redisstream.NewSubscriber(redisstream.SubscriberConfig{
+		Client: rdb,
+	}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	pub, err := redisstream.NewPublisher(redisstream.PublisherConfig{
+		Client: rdb,
+	}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	router.AddHandler(
+		"payment-handler",
+		"payment-completed",
+		sub,
+		"order-confirmed",
+		pub,
+		func(msg *message.Message) ([]*message.Message, error) {
+			var payment PaymentCompleted
+			if err := json.Unmarshal(msg.Payload, &payment); err != nil {
+				return nil, err
+			}
+			orderConfirmed := OrderConfirmed{
+				OrderID:     payment.OrderID,
+				ConfirmedAt: time.Now().Format(time.RFC3339),
+			}
+			payload, err := json.Marshal(orderConfirmed)
+			if err != nil {
+				return nil, err
+			}
+			return []*message.Message{message.NewMessage(watermill.NewUUID(), payload)}, nil
+		})
+
+	err = router.Run(context.Background())
+	if err != nil {
+		panic(err)
+	}
+}

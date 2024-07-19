65b9b6a1c566bb8e75952ba34e800f42131f1992
aerosystems <art_k0st@icloud.com>
2024-07-20 02:49:53 +0300

    done 05-events/04-project-cancellations

--- project/main.go
+++ project/main.go
@@ -88,7 +88,7 @@ func main() {
 		"TicketBookingConfirmed",
 		receiptsSub,
 		func(msg *message.Message) error {
-			var payload TicketBookingConfirmedPayload
+			var payload TicketBookingConfirmed
 			if err := json.Unmarshal(msg.Payload, &payload); err != nil {
 				return err
 			}
@@ -102,15 +102,30 @@ func main() {
 		})
 
 	router.AddNoPublisherHandler(
-		"spreadsheet-handler",
+		"spreadsheet-confirmed-handler",
 		"TicketBookingConfirmed",
 		spreadsheetsSub,
 		func(msg *message.Message) error {
-			var payload TicketBookingConfirmedPayload
+			var payload TicketBookingConfirmed
 			if err := json.Unmarshal(msg.Payload, &payload); err != nil {
 				return err
 			}
 			if err := spreadsheetsClient.AppendRow(context.Background(), "tickets-to-print", []string{payload.TicketID, payload.CustomerEmail, payload.Price.Amount, payload.Price.Currency}); err != nil {
+				return fmt.Errorf("error appending row for ticket %s: %v", payload.TicketID, err)
+			}
+			return nil
+		})
+
+	router.AddNoPublisherHandler(
+		"spreadsheet-canceled-handler",
+		"TicketBookingCanceled",
+		spreadsheetsSub,
+		func(msg *message.Message) error {
+			var payload TicketBookingCanceled
+			if err := json.Unmarshal(msg.Payload, &payload); err != nil {
+				return err
+			}
+			if err := spreadsheetsClient.AppendRow(context.Background(), "tickets-to-refund", []string{payload.TicketID, payload.CustomerEmail, payload.Price.Amount, payload.Price.Currency}); err != nil {
 				return fmt.Errorf("error appending row for ticket %s: %v", payload.TicketID, err)
 			}
 			return nil
@@ -128,8 +143,15 @@ func main() {
 		}
 
 		for _, ticket := range request.Tickets {
-			if err := pub.Publish("TicketBookingConfirmed", NewTicketBookingConfirmedMessage(ticket)); err != nil {
-				return err
+			switch ticket.Status {
+			case "confirmed":
+				if err := pub.Publish("TicketBookingConfirmed", NewTicketBookingConfirmedMessage(ticket)); err != nil {
+					return err
+				}
+			case "canceled":
+				if err := pub.Publish("TicketBookingCanceled", NewTicketBookingCanceledMessage(ticket)); err != nil {
+					return err
+				}
 			}
 		}
 
@@ -225,7 +247,7 @@ func (c SpreadsheetsClient) AppendRow(ctx context.Context, spreadsheetName strin
 	return nil
 }
 
-type Header struct {
+type EventHeader struct {
 	ID          string `json:"id"`
 	PublishedAt string `json:"published_at"`
 }
@@ -235,16 +257,16 @@ type Money struct {
 	Currency string `json:"currency"`
 }
 
-type TicketBookingConfirmedPayload struct {
-	Header        Header `json:"header"`
-	TicketID      string `json:"ticket_id"`
-	CustomerEmail string `json:"customer_email"`
-	Price         Money  `json:"price"`
+type TicketBookingConfirmed struct {
+	Header        EventHeader `json:"header"`
+	TicketID      string      `json:"ticket_id"`
+	CustomerEmail string      `json:"customer_email"`
+	Price         Money       `json:"price"`
 }
 
 func NewTicketBookingConfirmedMessage(ticket Ticket) *message.Message {
-	ticketBookingConfirmed := TicketBookingConfirmedPayload{
-		Header: Header{
+	ticketBookingConfirmed := TicketBookingConfirmed{
+		Header: EventHeader{
 			ID:          watermill.NewUUID(),
 			PublishedAt: time.Now().Format(time.RFC3339),
 		},
@@ -262,4 +284,33 @@ func NewTicketBookingConfirmedMessage(ticket Ticket) *message.Message {
 	}
 
 	return message.NewMessage(ticketBookingConfirmed.Header.ID, payload)
+}
+
+type TicketBookingCanceled struct {
+	Header        EventHeader `json:"header"`
+	TicketID      string      `json:"ticket_id"`
+	CustomerEmail string      `json:"customer_email"`
+	Price         Money       `json:"price"`
+}
+
+func NewTicketBookingCanceledMessage(ticket Ticket) *message.Message {
+	ticketBookingCanceled := TicketBookingCanceled{
+		Header: EventHeader{
+			ID:          watermill.NewUUID(),
+			PublishedAt: time.Now().Format(time.RFC3339),
+		},
+		TicketID:      ticket.TicketID,
+		CustomerEmail: ticket.CustomerEmail,
+		Price: Money{
+			Amount:   ticket.Price.Amount,
+			Currency: ticket.Price.Currency,
+		},
+	}
+
+	payload, err := json.Marshal(ticketBookingCanceled)
+	if err != nil {
+		panic(err)
+	}
+
+	return message.NewMessage(ticketBookingCanceled.Header.ID, payload)
 }
